328ab6ae156170e0187f950e3c8b39196b149456
kalkidan-solomon-coder <152845891+kalkidan-solomon-coder@users.noreply.github.com>
2024-05-14 18:07:56 +0300

    change

--- new-project/src/Clock.jsx
+++ new-project/src/Clock.jsx
@@ -51,29 +51,23 @@ function reset(){
   return (
     <>
     <div className="bg-zinc-600 lg:w-1/2 md:w-2/3 sm:w-2/3 lg:ml-72 md:ml-32 sm:ml-28 mt-10 rounded shadow-sm shadow-black">
-        <h1 className="text-red-400 sm:pl-16 md:pl-36 lg:pl-40 pt-14 pb-14 inline-block font-mono font-semibold text-6xl ">
-          {hourse<10?"0"+hourse:hourse}:
+        <h1 className="text-red-400 sm:pl-24 md:pl-36 md:ml-1 lg:pl-40 pt-14 pb-14 ml-16 inline-block font-mono font-semibold text-6xl lg:ml-6 ">
+          {hourse<10?"0"+hourse:hourse}:{minutes<10?"0"+minutes:minutes}:{seconds<10?"0"+seconds:seconds}
         </h1>
-        <h1 className="text-red-400 sm:pl-4 md:pl-4 lg:pl-3 pt-14 pb-14 inline-block font-mono font-semibold text-6xl">
-          {minutes<10?"0"+minutes:minutes}:
-        </h1>
-        <h1 className="text-red-400 sm:pl-4 md:pl-4 lg:pl-3 pt-14 pb-14 inline-block font-mono font-semibold text-6xl">
-          {seconds<10?"0"+seconds:seconds}
-        </h1>
-        <div className="flex justify-between lg:ml-44 md:ml-40 sm:ml-28 md:ml-32  text-xl -mt-10 pb-10">
+        <div className="flex justify-between lg:ml-40 md:ml-36 sm:ml-20  md:ml-32  text-xl -mt-10 pb-10">
           <button
-            className="bg-green-600 text-yellow-300 font-semibold px-5 py-1 rounded cursor-pointer"
+            className="bg-green-600 text-yellow-300 font-semibold px-5 py-1 rounded cursor-pointer ml-14 lg:ml-2 md:ml-2"
            onClick={start} 
           >
             Start
           </button>
           <button
-            className="lg:-ml-44 sm:ml-1 bg-green-600  text-yellow-300 font-semibold px-5 rounded cursor-pointer"
+            className="lg:-ml-44 sm:ml-1 bg-green-600  text-yellow-300 font-semibold px-5 rounded cursor-pointer -ml-1"
             onClick={stop}
           >
             Stop
           </button>
-          <button className="bg-green-600  text-yellow-300 font-semibold px-4 lg:-ml-44 sm:ml-1 md:mr-56 lg:mr-44  sm:mr-40 rounded cursor-pointer" onClick={reset}>
+          <button className="bg-green-600  text-yellow-300 font-semibold px-4 lg:-ml-44 sm:ml-1 md:mr-56 lg:mr-40  sm:mr-40 rounded cursor-pointer mr-20" onClick={reset}>
             Reset
           </button>
         </div>
